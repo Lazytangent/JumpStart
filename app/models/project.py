d388c770f9b28328d99875d18c1874df9159a5b8
Lazytangent <pm123633@gmail.com>
2021-02-20 14:38:06 -0600

    Create projects table

---
+++ app/models/project.py
@@ -0,0 +1,14 @@
+from .db import db
+
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey("users.id"))
+    thumbnailImgUrl = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.Text, nullable=False)
+    goalAmount = db.Column(db.Integer, nullable=False)
+    minPledge = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User", back_populates="projects")
