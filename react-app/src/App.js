f8879a4ad5cffc6b6701e8f6178529ca808c7eaf
Jesse <jbenlind@gmail.com>
2021-02-21 17:37:47 -0600

    set up signup modal

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
