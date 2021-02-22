b67fe83f51a4eb609e9f747c2c68d0b67ce60f56
Jesse <jbenlind@gmail.com>
2021-02-22 11:07:23 -0600

    changes to users seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -2,11 +2,18 @@ from werkzeug.security import generate_password_hash
 from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
+
+
 def seed_users():
 
     demo = User(username='Demo', email='demo@aa.io',
-                password='password', city='Dallas',
-                state='Texas')
+                password='password', city='Dallas', state='Texas')
+
+    Jesse = User(username='jesse' email='jesse@jumpstart.io',
+                 password='password', city='Milwaukee', state='Wisconsin')
+
+    Peter = User(username='peter' email='peter@jumpstart.io',
+                 password='password', city='Dallas', state='Texas')
 
     db.session.add(demo)
 
@@ -16,6 +23,8 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users;')
     db.session.commit()
