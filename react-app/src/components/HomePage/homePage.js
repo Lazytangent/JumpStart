fc5985534f60c30c1bb5f5b1288265a97f82ed17
Dillon <dft609@gmail.com>
2021-02-23 15:41:03 -0700

    state abbreviations

--- react-app/src/components/HomePage/homePage.js
+++ react-app/src/components/HomePage/homePage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
 import { getHomePageProjects } from '../../store/project.js'
-// import csc from "country-state-city"
+import csc from "country-state-city"
 import "./homePage.css"
 
 
@@ -23,8 +23,19 @@ const HomePage = () => {
 
     }
 
-    // let stateName;
-    // let allStates = csc.getStatesOfCountry('US')
+    const getStateAbbreviation = (project) => {
+        let result;
+        const allStates = csc.getStatesOfCountry('US')
+
+        let stateName = project.user.state;
+
+        allStates.forEach((state) => {
+            if (state.name === stateName) {
+                result = state.isoCode
+            }
+        })
+        return result;
+    }
 
     return (
         <div className="homePage">
@@ -39,23 +50,23 @@ const HomePage = () => {
                 <div className="homePage-grid-most-popular">
                     <div id="homePage-project-grid">
                         {mostPopular &&
-                        mostPopular.map((project) => (
-                            <Link id='homePage-project-card-link' key={project.id} to={`${project.id}`}>
-                                <div id="homePage-project-card" value={project.id}>
+                            mostPopular.map((project) => (
+                                <Link id='homePage-project-card-link' key={project.id} to={`${project.id}`}>
+                                    <div id="homePage-project-card" value={project.id}>
                                         <div>
                                             {/* <img>{project.thumbnailImgUrl}</img> */}
                                         </div>
-                                        <div id="projectCard-location">{`${project.user.city},${project.user.state}`}</div>
+                                        <div id="projectCard-location">{`${project.user.city},${getStateAbbreviation(project)}`}</div>
                                         <div id="projectCard-title">{project.name}</div>
                                         <div>{project.description}</div>
                                         {/* limit project description to two lines */}
                                         <div>
                                             {/* <span id="progressBar" style=width:getPercentage(project)></span> */}
                                         </div>
-                                </div>
-                            </Link>
-                        ))}
-                         <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
+                                    </div>
+                                </Link>
+                            ))}
+                        <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
                     </div>
                 </div>
                 <div className="homePage-grid-most-recent">
@@ -63,47 +74,47 @@ const HomePage = () => {
                         {mostRecent &&
                             mostRecent.map((project) => (
                                 <Link id='homePage-project-card-link' key={project.id} to={`${project.id}`}>
-                                <div id="homePage-project-card" value={project.id}>
+                                    <div id="homePage-project-card" value={project.id}>
                                         <div>
                                             {/* <img>{project.thumbnailImgUrl}</img> */}
                                         </div>
-                                        <div>{`${project.user.city},${project.user.state}`}</div>
+                                        <div>{`${project.user.city},${getStateAbbreviation(project)}`}</div>
                                         <div>{project.name}</div>
                                         <div>{project.description}</div> two lines
                                         <div>
                                             {/* <span id="progressBar" style={`width: ${}`}></span> */}
                                         </div>
-                                </div>
-                            </Link>
+                                    </div>
+                                </Link>
                             ))}
-                         <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
+                        <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
                     </div>
                 </div>
                 <div className="homePage-grid-near-you">
                     <div id="homePage-project-grid">
-                    {trending &&
+                        {trending &&
                             trending.map((project) => (
                                 <Link id='homePage-project-card-link' key={project.id} to={`${project.id}`}>
-                                <div id="homePage-project-card" value={project.id}>
+                                    <div id="homePage-project-card" value={project.id}>
                                         <div>
                                             {/* <img>{project.thumbnailImgUrl}</img> */}
                                         </div>
-                                        <div>{`${project.user.city},${project.user.state}`}</div>
+                                        <div>{`${project.user.city},${getStateAbbreviation(project)}`}</div>
                                         <div>{project.name}</div>
                                         <div>{project.description}</div> two lines
                                         <div>
                                             {/* <span id="progressBar" style=`width: ${}`></span> */}
                                         </div>
-                                </div>
-                            </Link>
+                                    </div>
+                                </Link>
                             ))}
-                         <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
+                        <Link id="homePage-see-more" to=''>See more<i id="homePage-right-arrow" className="far fa-arrow-alt-circle-right"></i></Link>
                     </div>
                 </div>
             </div>
         </div>
 
-  );
+    );
 
 }
 
