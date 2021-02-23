dda7319f324fe71954c3faefd89b09e48c1b0214
Jesse <jbenlind@gmail.com>
2021-02-23 17:16:28 -0600

    added file location changes

--- react-app/src/components/auth/LogoutButton/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton/LogoutButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
-import { logout } from '../../store/session';
+import { logout } from '../../../store/session';
 
 const LogoutButton = ({setAuthenticated}) => {
   const dispatch = useDispatch();
