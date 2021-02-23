46d6cf199564e5efc0e20d5f6f7f50a513ad3a6d
Jesse <jbenlind@gmail.com>
2021-02-22 21:42:46 -0600

    grid layout

--- react-app/src/components/HomePage/homePage.js
+++ react-app/src/components/HomePage/homePage.js
@@ -10,6 +10,26 @@ const HomePage = () => {
             <div className="homePage-image-container">
                 <img className="homePage-image" src="homePage1.jpg" alt=""></img>
             </div>
+            <div className="homePage-description-box">
+                <img className="homePage-description-image" src="homePage-description.jpg" alt=""></img>
+            </div>
+            <div className="homePage-grid">
+                <div className="homePage-grid-most-recent">
+                    <div id="homePage-project-grid">
+                        {/* map over the projects based upon query */}
+                    </div>
+                </div>
+                <div className="homePage-grid-most-popular">
+                    <div id="homePage-project-grid">
+                        {/* map over the projects based upon query */}
+                    </div>
+                </div>
+                <div className="homePage-grid-near-you">
+                    <div id="homePage-project-grid">
+                        {/* map over the projects based upon query */}
+                    </div>
+                </div>
+            </div>
         </div>
 
   );
