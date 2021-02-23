19ec2551bb6c8ee3d05c4e3be57a7a59935ba68a
ThreedTX <ReedWinstonNorton@gmail.com>
2021-02-23 13:12:46 -0600

    After breaking a lot of stuff... back to simple search state

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,43 +1,69 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux'
-import LoginForm from '../auth/LoginForm'
-import SignUpForm from '../auth/SignUpForm'
-import LogoutButton from '../auth/LogoutButton';
-import { useModalContext } from '../../context/Modal';
-import CreateProject from '../CreateProject'
-import './NavBar.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../auth/SignUpForm";
+import LogoutButton from "../auth/LogoutButton";
+import SearchBar from "../SearchBar/SearchBar";
+import { useModalContext } from "../../context/Modal";
+import CreateProject from "../CreateProject";
+import "./NavBar.css";
 
 const NavBar = ({ setAuthenticated, setShowModal }) => {
-  const { showLoginModal, setShowLoginModal, showSignUpModal, setShowSignUpModal } = useModalContext();
-  const user = useSelector(state => state.session.user)
+  const {
+    showLoginModal,
+    setShowLoginModal,
+    showSignUpModal,
+    setShowSignUpModal,
+    showSearchBarModal,
+    setShowSearchBarModal,
+  } = useModalContext();
+  const user = useSelector((state) => state.session.user);
 
-  console.log(user)
+  console.log(user);
 
   return (
     <div>
       <ul className="navBar">
         <div className="navBar-first-fraction">
           <div>
-            {!user && <button onClick={() => {
-              setShowSignUpModal(false)
-              setShowLoginModal((prev) => !prev)
-            }}>Login</button>}
+            {!user && (
+              <button
+                onClick={() => {
+                  setShowSignUpModal(false);
+                  setShowLoginModal((prev) => !prev);
+                }}
+              >
+                Login
+              </button>
+            )}
             {showLoginModal && <LoginForm />}
           </div>
           <div>
-            {!user && <button onClick={() => {
-              setShowLoginModal(false)
-              setShowSignUpModal((prev) => !prev)
-            }}>Sign Up</button>}
+            {!user && (
+              <button
+                onClick={() => {
+                  setShowLoginModal(false);
+                  setShowSignUpModal((prev) => !prev);
+                }}
+              >
+                Sign Up
+              </button>
+            )}
             {showSignUpModal && <SignUpForm />}
           </div>
         </div>
         <div className="navBar-second-fraction">
-          <NavLink className="navBar-home" to="/" exact={true} activeClassName="active" onClick={() => {
-            setShowSignUpModal(false)
-            setShowLoginModal(false)
-            }}>
+          <NavLink
+            className="navBar-home"
+            to="/"
+            exact={true}
+            activeClassName="active"
+            onClick={() => {
+              setShowSignUpModal(false);
+              setShowLoginModal(false);
+            }}
+          >
             JumpStart<img className="navBar-logo" src="logo.png" alt=""></img>
           </NavLink>
         </div>
@@ -47,11 +73,26 @@ const NavBar = ({ setAuthenticated, setShowModal }) => {
           </div>
         </div>
         <div>
+          {
+            <button
+              onClick={() => {
+                setShowSignUpModal(false);
+                setShowLoginModal(false);
+                setShowSearchBarModal((prev) => !prev);
+              }}
+            >
+              Search
+            </button>
+          }
+          {showSearchBarModal && <SearchBar />}
+        </div>
+
+        <div>
           {user && <LogoutButton setAuthenticated={setAuthenticated} />}
         </div>
       </ul>
-    </div >
+    </div>
   );
-}
+};
 
 export default NavBar;
---
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+import { Modal, useModalContext } from "../../context/Modal";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  const { showSearchBarModal, setShowSearchBarModal } = useModalContext();
+  const [search, setSearch] = useState("");
+  const [matches, setMatches] = useState("");
+
+  const projects = [
+    {
+      userId: 2,
+      name: "Reed is the coding G.O.A.T.",
+      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
+      description: "Big facts",
+      goalAmount: 0,
+      minPledge: 10000,
+    },
+    {
+      userId: 3,
+      name: "Peter needs coding lessons",
+      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
+      description: "I'm Peter and I need money because I'm the worst",
+      goalAmount: 10000000,
+      minPledge: 1,
+    },
+    {
+      userId: 1,
+      name: "Dillon needs $ for ballroom dancing",
+      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
+      description: "I just want to dance",
+      goalAmount: 1000,
+      minPledge: 4,
+    },
+    {
+      userId: 3,
+      name: "Jesse is dying",
+      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
+      description: "Need money for new legs",
+      goalAmount: 10040,
+      minPledge: 3,
+    },
+    {
+      userId: 3,
+      name: "Trump needs $ for a wall",
+      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
+      description: "Big Wall. Need money.",
+      goalAmount: 9999999,
+      minPledge: 2,
+    },
+  ];
+  const searchProjects = async (searchText) => {
+    let projectMatches = projects.filter((project) => {
+      const regex = new RegExp(`^${searchText}`, "gi");
+      return project.name.match(regex);
+    });
+
+    if (searchText.length === 0) {
+      projectMatches = [];
+    }
+
+    setMatches(projectMatches);
+  };
+
+  return (
+    <>
+      {showSearchBarModal && (
+        <Modal onClose={() => setShowSearchBarModal(false)}>
+          <div className="searchBar-container">
+            <input
+              type="search"
+              name="search"
+              className="searchBar-input"
+              placeholder="Search by project or location?"
+              value={search}
+              onChange={(e) => {
+                searchProjects(e.target.value);
+                setSearch(e.target.value);
+              }}
+            />
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default SearchBar;
