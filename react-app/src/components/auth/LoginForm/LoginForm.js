fb2a1f5a1dee41d7519e4543f83502a1299bc5a6
Lazytangent <pm123633@gmail.com>
2021-02-24 12:24:56 -0600

    Really basic styling for login form

--- react-app/src/components/auth/LoginForm/LoginForm.js
+++ react-app/src/components/auth/LoginForm/LoginForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 
-import styles from './LoginForm.module.css';
-import { login } from '../../../store/session';
-import { Modal, useModalContext } from '../../../context/Modal';
+import styles from "./LoginForm.module.css";
+import { login } from "../../../store/session";
+import { Modal, useModalContext } from "../../../context/Modal";
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const { showLoginModal, setShowLoginModal } = useModalContext();
@@ -15,7 +15,7 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
 
   const onLogin = async (e) => {
     e.preventDefault();
-    const user = await dispatch(login(email, password))
+    const user = await dispatch(login(email, password));
     if (!user.errors) {
       setShowLoginModal(false);
     } else {
@@ -37,41 +37,36 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
 
   return (
     <>
-      {showLoginModal &&
+      {showLoginModal && (
         <Modal onClose={() => setShowLoginModal(false)}>
           <form className={styles.form} onSubmit={onLogin}>
-            <button id="close-button" className={styles.closeBtn} onClick={() => setShowLoginModal((prev) => !prev)}><i id="close-icon" className="far fa-window-close"></i></button>
+            <div className={styles.closeBtnContainer}>
+              <button id="close-button" className={styles.closeBtn} onClick={() => setShowLoginModal((prev) => !prev)}>
+                <i id="close-icon" className="far fa-window-close"></i>
+              </button>
+            </div>
+            <div className={styles.title}>
+              <h2 className={styles.header}>Log In</h2>
+            </div>
             <div>
               {errors.map((error, idx) => (
                 <li key={idx}>{error}</li>
               ))}
             </div>
             <div>
-              <input
-                name="email"
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={updateEmail}
-                className={styles.formField}
-              />
+              <input name="email" type="text" placeholder="Email" value={email} onChange={updateEmail} className={styles.formField} />
             </div>
             <div>
-              <input
-                name="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={updatePassword}
-                className={styles.formField}
-              />
+              <input name="password" type="password" placeholder="Password" value={password} onChange={updatePassword} className={styles.formField} />
             </div>
             <div className={styles.btnContainer}>
-              <button id="submit-button" className={styles.submitBtn}>Login</button>
+              <button id="submit-button" className={styles.submitBtn}>
+                Login
+              </button>
             </div>
           </form>
         </Modal>
-      }
+      )}
     </>
   );
 };
