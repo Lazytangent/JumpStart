e94fe61880605cf41d5da9e6e46fd8b71d893669
ThreedTX <ReedWinstonNorton@gmail.com>
2021-02-23 18:40:10 -0600

    SearchBar OCD CSS

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import logo_30x30 from "../../../public/logo_30x30.png";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import logo_30x30 from "./logo_30x30.png";
+import { getHomePageProjects } from "../../store/project";
 import { SearchModal, useModalContext } from "../../context/Modal";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
   const { showSearchBarModal, setShowSearchBarModal } = useModalContext();
   const [search, setSearch] = useState("");
   const [matches, setMatches] = useState("");
+
+  function focusSearchBar() {
+    document.getElementById("searchModalInput").focus();
+  }
 
   const projects = [
     {
@@ -36,7 +43,6 @@ const SearchBar = () => {
     {
       userId: 3,
       name: "Jesse is dying",
-      thumbnailImgUrl: "https://i.imgur.com/xg4C3F2.jpg",
       description: "Need money for new legs",
       goalAmount: 10040,
       minPledge: 3,
@@ -48,6 +54,20 @@ const SearchBar = () => {
       description: "Big Wall. Need money.",
       goalAmount: 9999999,
       minPledge: 2,
+    },
+    {
+      userId: 2,
+      name: "Juliet was in hang gliding accident",
+      description: "Need money for surgery and my dog Juice",
+      goalAmount: 33555,
+      minPledge: 4,
+    },
+    {
+      userId: 3,
+      name: "Justin shattered femur skateboarding",
+      description: "Kickflips are hard man.",
+      goalAmount: 11555,
+      minPledge: 3,
     },
   ];
   const searchProjects = async (searchText) => {
@@ -63,41 +83,58 @@ const SearchBar = () => {
     setMatches(projectMatches);
   };
 
+  useEffect(() => {
+    focusSearchBar();
+    getHomePageProjects("popular");
+  }, [dispatch]);
   return (
     <>
       {showSearchBarModal && (
         <SearchModal onClose={() => setShowSearchBarModal(false)}>
-          <div className="searchBar-container">
-            <input
-              type="search"
-              name="search"
-              className="searchBar-input"
-              placeholder="Search by project or location?"
-              value={search}
-              onChange={(e) => {
-                searchProjects(e.target.value);
-                setSearch(e.target.value);
-              }}
-            />
-          </div>
-          <div>
-            {matches &&
-              matches.map((project) => (
-                <li className="searchBar-matches">
-                  {!project.thumbnailImgUrl && (
-                    <div>
-                      <img
-                        className="logo_30x30"
-                        src={logo_30x30}
-                        alt="A small heart with an EKG graph of the electrical impulses that move through the heart displayed inside of it."
-                      />
-                    </div>
-                  )}
-                  <div>
-                    <p>{project.name}</p>
-                  </div>
-                </li>
-              ))}
+          <div className="searchBarMatches-container">
+            <div className="searchBar-container">
+              <button
+                id="inputClose-button"
+                onClick={() => setShowSearchBarModal((prev) => !prev)}
+              >
+                <i id="inputClose-icon" className="fa fa-times fa-2x"></i>
+              </button>
+              <input
+                id="searchModalInput"
+                type="search"
+                name="search"
+                className="searchBar-input"
+                placeholder="Search by project or location?"
+                value={search}
+                onChange={(e) => {
+                  searchProjects(e.target.value);
+                  setSearch(e.target.value);
+                }}
+                autofocus
+              />
+            </div>
+            <div>
+              {matches &&
+                matches.map((project) => (
+                  <li className="searchBarMatches">
+                    {!project.thumbnailImgUrl && (
+                      <div>
+                        <img
+                          className="logo_30x30"
+                          src={logo_30x30}
+                          alt="A small heart with an EKG graph of the electrical impulses that move through the heart displayed inside of it."
+                        />
+                      </div>
+                    )}
+                    {search && (
+                      <div className="searchBarMatches-content">
+                        <p className="projectName">{project.name}</p>
+                        <p>{project.description}</p>
+                      </div>
+                    )}
+                  </li>
+                ))}
+            </div>
           </div>
         </SearchModal>
       )}
