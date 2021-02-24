dec2d6b52dd3db80e38e10e9bd5b15d49170d9b4
Dillon <dft609@gmail.com>
2021-02-24 15:27:09 -0700

    image problems

--- react-app/src/components/ProjectPage/projectPage.js
+++ react-app/src/components/ProjectPage/projectPage.js
@@ -20,11 +20,30 @@ const ProjectPage = () => {
 
   return (
     <>
-      {project && <div>
-        <h1>{project.name}</h1>
-        <img src={project.thumbnailImgUrl} className='thumbnail'></img>
-        <div>{project.description}</div>
-      </div>
+      {project &&
+        <div className="project-container">
+          {/* <h1 className="project-header">{project.name}</h1>
+          <img src={project.thumbnailImgUrl} className='thumbnail'></img>
+          <div>{project.description}</div> */}
+          <h1>Grid Layout</h1>
+
+          <p>This grid layout contains six columns and three rows:</p>
+
+          <div class="grid-container">
+            <div class="item1">Header</div>
+            <div class="item2">Menu</div>
+            <div class="item3">'
+              <div className=" item3 thumbnail">
+                <img src={project.thumbnailImgUrl}></img>
+              </div>
+            </div>
+            {/* <div class="item3">Main</div> */}
+            <div class="item4">Donations</div>
+            <div class="item5">Footer</div>
+            <div class="item6">Another Footer</div>
+
+          </div>
+        </div>
       }
     </>
   )
