3458ec3259dc316541f185902a461db004b1195c
Lazytangent <pm123633@gmail.com>
2021-02-25 11:40:27 -0600

    Create CreateDonation form

--- app/api/donation_routes.py
+++ app/api/donation_routes.py
@@ -4,3 +4,8 @@ from app.forms import CreateDonation
 from app.models import db, Donation, Project, User
 
 donation_routes = Blueprint('donations', __name__)
+
+
+@donation_routes.route('/', methods=["POST"])
+def create_donation():
+    pass
---
+++ app/forms/create_donation.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class CreateDonation(FlaskForm):
+    userId = IntegerField("userId")
+    projectId = IntegerField("projectId")
+    donationAmount = IntegerField("donationAmount")
+    comment = StringField("comment")
+    anonymous = BooleanField("anonymous")
