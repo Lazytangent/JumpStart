659dbe948f0acada2fca82a097310231ac2ed880
Jesse <jbenlind@gmail.com>
2021-02-25 10:25:10 -0600

    more

--- react-app/src/components/DiscoverPage/discoverPage.js
+++ react-app/src/components/DiscoverPage/discoverPage.js
@@ -8,7 +8,9 @@ const DiscoverPage = () => {
             <Navigation />
             <div className="discoverPage">
                 <h1 className="discoverPage-header">Start helping those in need</h1>
-                
+                <div className="discoverPage-grid">
+
+                </div>
             </div>
         </>
     )
