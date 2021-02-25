aa79c5b97d61607025c718153ca2951ca754267d
Lazytangent <pm123633@gmail.com>
2021-02-25 11:47:34 -0600

    Draft basic post route for create donation

--- app/api/donation_routes.py
+++ app/api/donation_routes.py
@@ -1,11 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from app.forms import CreateDonation
 from app.models import db, Donation, Project, User
+from app.helpers import validation_errors_to_error_messages
 
 donation_routes = Blueprint('donations', __name__)
 
 
 @donation_routes.route('/', methods=["POST"])
 def create_donation():
-    pass
+    form = CreateDonation()
+    form['csrf_token'] = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        donation = Donation()
+        form.populate_obj(donation)
+        db.session.add(donation)
+        db.session.commit()
+        return donation.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}
