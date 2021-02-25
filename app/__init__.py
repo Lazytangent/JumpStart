3b493400605fe4ae6f8bf9abdc1e9ddc486b1f6b
Lazytangent <pm123633@gmail.com>
2021-02-25 11:34:02 -0600

    Create blueprint and import into init

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.project_routes import project_routes
+from .api.donation_routes import donation_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(project_routes, url_prefix='/api/projects')
+app.register_blueprint(donation_routes, url_prefix='/api/donations')
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/donation_routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+from app.forms import CreateDonation
+from app.models import db, Donation, Project, User
+
+donation_routes = Blueprint('donations', __name__)
