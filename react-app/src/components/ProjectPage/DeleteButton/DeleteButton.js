0a31b571633db7dccc14148563a4d32e18051ee0
Lazytangent <pm123633@gmail.com>
2021-02-26 13:25:02 -0600

    Just starting

---
+++ react-app/src/components/ProjectPage/DeleteButton/DeleteButton.js
@@ -0,0 +1,11 @@
+import styles from './DeleteButton.module.css';
+
+const DeleteButton = () => {
+  return (
+    <>
+      <h1>Delete this project</h1>
+    </>
+  );
+};
+
+export default DeleteButton;
