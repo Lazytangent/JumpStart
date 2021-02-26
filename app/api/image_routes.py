4cc54079bbcce8d436404c43921640d93ec15612
Lazytangent <pm123633@gmail.com>
2021-02-26 16:22:00 -0600

    Create thunk and backend api route for deleting an image by id

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -7,3 +7,12 @@ from app.helpers import allowed_file, upload_file_to_s3, \
 from app.models import Project, Image, db
 
 image_routes = Blueprint('images', __name__)
+
+
+@image_routes.route('/<int:image_id', methods=["DELETE"])
+def delete_image(image_id):
+    image = Image.query.get(image_id)
+    project = Project.query.get(image.projectId)
+    db.session.delete(image)
+    db.session.commit()
+    return project.to_dict()
