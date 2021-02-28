70dd6aad0c6c44e34d3e85f33059db809b5e6994
Jesse <jbenlind@gmail.com>
2021-02-28 15:12:35 -0600

    added footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -36,36 +36,36 @@ const App = () => {
 
   return (
     <>
-      <Switch>
-        <Route path="/login" exact={true}>
-          <LoginForm
-            authenticated={authenticated}
-            setAuthenticated={setAuthenticated}
-          />
-        </Route>
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-        </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-          <User />
-        </ProtectedRoute>
+        <Switch>
+          <Route path="/login" exact={true}>
+            <LoginForm
+              authenticated={authenticated}
+              setAuthenticated={setAuthenticated}
+            />
+          </Route>
+          <Route path="/sign-up" exact={true}>
+            <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
+          </Route>
+          <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
+            <UsersList />
+          </ProtectedRoute>
+          <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+            <User />
+          </ProtectedRoute>
 
-        <Route path="/" exact={true} authenticated={authenticated}>
-          <HomePage setAuthenticated={setAuthenticated} />
-        </Route>
-        <Route path="/discover" exact={true} authenticated={authenticated}>
-          <DiscoverPage setAuthenticated={setAuthenticated} />
-        </Route>
-        <Route path="/new-project" exact={true} authenticated={authenticated}>
-          <CreateProject setAuthenticated={setAuthenticated} />
-        </Route>
-        <Route path="/:projectId" exact={true} authenticated={authenticated}>
-          <ProjectPage setAuthenticated={setAuthenticated} />
-        </Route>
-      </Switch>
+          <Route path="/" exact={true} authenticated={authenticated}>
+            <HomePage setAuthenticated={setAuthenticated} />
+          </Route>
+          <Route path="/discover" exact={true} authenticated={authenticated}>
+            <DiscoverPage setAuthenticated={setAuthenticated} />
+          </Route>
+          <Route path="/new-project" exact={true} authenticated={authenticated}>
+            <CreateProject setAuthenticated={setAuthenticated} />
+          </Route>
+          <Route path="/:projectId" exact={true} authenticated={authenticated}>
+            <ProjectPage setAuthenticated={setAuthenticated} />
+          </Route>
+        </Switch>
       <Footer/>
     </>
   );
